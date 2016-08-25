648a6f818d5fddcdabe52594b5df34ea54cc34a7
GoesToEleven <tuddleymc@gmail.com>
2016-08-25 10:20:00 -0700

    adds new template examples

---
+++ 009_func/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strings"
+	"text/template"
+)
+
+var tpl *template.Template
+
+type sage struct {
+	Name  string
+	Motto string
+}
+
+type car struct {
+	Manufacturer string
+	Model        string
+	Doors        int
+}
+
+// create a FuncMap to register functions.
+var funcMap = template.FuncMap{
+	// "uc" is what the function will be called in the template
+	"uc": strings.ToUpper,
+	"ff": firstFive,
+}
+
+func init() {
+	tpl = template.Must(template.New("").Funcs(funcMap).ParseFiles("tpl.gohtml"))
+}
+
+func firstFive(s string) string {
+	s = strings.TrimSpace(s)
+	s = s[:3]
+	return s
+}
+
+
+func main() {
+
+	b := sage{
+		Name:  "Buddha",
+		Motto: "The belief of no beliefs",
+	}
+
+	g := sage{
+		Name:  "Gandhi",
+		Motto: "Be the change",
+	}
+
+	m := sage{
+		Name:  "Martin Luther King",
+		Motto: "Hatred never ceases with hatred but with love alone is healed.",
+	}
+
+	f := car{
+		Manufacturer: "Ford",
+		Model:        "F150",
+		Doors:        2,
+	}
+
+	c := car{
+		Manufacturer: "Toyota",
+		Model:        "Corolla",
+		Doors:        4,
+	}
+
+	sages := []sage{b, g, m}
+	cars := []car{f, c}
+
+	data := struct {
+		Wisdom    []sage
+		Transport []car
+	}{
+		sages,
+		cars,
+	}
+
+	err := tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", data)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
