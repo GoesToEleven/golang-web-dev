96bde994c285335ae97a29504b6d922169997ef6
GoesToEleven <tuddleymc@gmail.com>
2016-09-28 21:44:48 -0700

    in class files

---
+++ 000_temp/14_nested-templates/01/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"html/template"
+	"os"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+
+func main() {
+	tpl.ExecuteTemplate(os.Stdout, "footerThing", nil)
+	tpl.ExecuteTemplate(os.Stdout, "headerThing", nil)
+}
---
+++ 000_temp/15_tcp-server/02/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net"
+	"log"
+	"bufio"
+	"fmt"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", ":8080")
+	if err != nil {
+		log.Println(err)
+	}
+
+	fmt.Fprintln(conn, "hello from calling in")
+
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+		break
+	}
+	conn.Close()
+}
