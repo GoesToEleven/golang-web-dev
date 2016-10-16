62f4acbbaffb523a6777b6404c6b3bf4e8716643
GoesToEleven <tuddleymc@gmail.com>
2016-10-16 13:05:57 -0700

    prepping

---
+++ 028_passing-data_RECORD/06_enctype_RECORD/03_text/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"fmt"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+type person struct {
+	FirstName  string
+	LastName   string
+	Subscribed bool
+}
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func foo(w http.ResponseWriter, req *http.Request) {
+
+	// method
+	fmt.Println("\nMETHOD:", req.Method)
+	// URL
+	fmt.Println("URI:", req.URL.Path)
+	// body
+	bs := make([]byte, req.ContentLength)
+	req.Body.Read(bs)
+	fmt.Println("BODY:", string(bs))
+
+	f := req.FormValue("first")
+	l := req.FormValue("last")
+	s := req.FormValue("subscribe") == "on"
+
+	err := tpl.ExecuteTemplate(w, "index.gohtml", person{f, l, s})
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		log.Fatalln(err)
+	}
+}
--- 029_redirect/04_WriteHeader_FIX/main.go
+++ 029_redirect/04_WriteHeader_FIX/main.go
@@ -27,9 +27,8 @@ func foo(w http.ResponseWriter, req *http.Request) {
 
 func bar(w http.ResponseWriter, req *http.Request) {
 	fmt.Println("Your request method at bar:", req.Method)
-	//http.Redirect(w, req, "/", http.StatusSeeOther)
-	w.Header().Set("Location", "/")
-	w.WriteHeader(http.StatusSeeOther)
+	http.Redirect(w, req, "/", http.StatusTemporaryRedirect)
+	//http.Redirect(w, req, "/", 307)
 }
 
 func barred(w http.ResponseWriter, req *http.Request) {
