7c10984e3eae2ad5bcf56526098a8113f4b0262e
GoesToEleven <tuddleymc@gmail.com>
2017-01-04 21:35:05 -0800

    you're doing great

---
+++ 041_ajax/02/01/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+	"fmt"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/foo", foo)
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+	s := `Here is some text from foo`
+	fmt.Fprintln(w, s)
+}
