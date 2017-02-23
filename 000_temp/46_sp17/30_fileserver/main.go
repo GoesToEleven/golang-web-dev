1a3394ce942ab89240d51ec54f0c71b60e4c5577
GoesToEleven <tuddleymc@gmail.com>
2017-02-22 20:35:42 -0800

    class

---
+++ 000_temp/46_sp17/30_fileserver/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", dog)
+	http.Handle("/resources/", http.FileServer(http.Dir("./assets")))
+	http.ListenAndServe(":8080", nil)
+}
+
+func dog(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(w, `<img src="/resources/toby.jpg">`)
+}
