abbe53a1f452cb6bbf831dbd947fd1aa04f2ecd5
Todd McLeod <tuddleymc@gmail.com>
2017-05-03 20:56:05 -0700

    you're doing great

---
+++ 000_temp/46_sp17/49_multiple-redirects/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+	"html/template"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/foo", foo)
+	http.HandleFunc("/bar", bar)
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+	return // comment out and experiment to see what happens
+	http.Redirect(w, r, "/bar", http.StatusSeeOther)
+}
+
+func bar(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "bar.gohtml", nil)
+}
---
+++ 000_temp/46_sp17/50_mongo/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"net/http"
+	"html/template"
+	"gopkg.in/mgo.v2"
+	"fmt"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type customer struct {
+	Name string `json:"name" bson:"name"`
+}
+
+var tpl *template.Template
+var DB *mgo.Database
+var Customer *mgo.Collection
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+
+	// get a mongo sessions
+	//s, err := mgo.Dial("mongodb://bond:moneypenny007@localhost/bookstore")
+	s, err := mgo.Dial("mongodb://localhost/bookstore")
+	if err != nil {
+		panic(err)
+	}
+
+	if err = s.Ping(); err != nil {
+		panic(err)
+	}
+
+	DB = s.DB("bookstore")
+	Customer = DB.C("customer")
+
+	fmt.Println("You connected to your mongo database.")
+}
+
+
+func main() {
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	customers := []customer{}
+	err := Customer.Find(bson.M{}).All(&customers)
+	if err != nil {
+		fmt.Println(err)
+	}
+	tpl.ExecuteTemplate(w, "index.gohtml", customers)
+}
