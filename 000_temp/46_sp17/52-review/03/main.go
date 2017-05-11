1621d7f31362644c1503ce8beab92a318d5dec21
Todd McLeod <tuddleymc@gmail.com>
2017-05-10 18:32:19 -0700

    you're doing great

---
+++ 000_temp/46_sp17/52-review/03/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+	"io"
+)
+
+func main() {
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type","text/plain; charset=utf-8")
+	io.WriteString(w, `<h1>Hello World</h1>`)
+}
---
+++ 000_temp/46_sp17/52-review/04/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+	"html/template"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+}
