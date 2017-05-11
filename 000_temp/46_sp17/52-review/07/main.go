7967a2936786e0d87fe1043bf0d882812f846900
Todd McLeod <tuddleymc@gmail.com>
2017-05-10 20:44:01 -0700

    you're doing great

---
+++ 000_temp/46_sp17/52-review/07/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"net/http"
+	"html/template"
+)
+
+type person struct {
+	First string
+	Last string
+	Saying string
+}
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(http.Dir("public"))))
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	p1 := person{
+		First: "James",
+		Last: "Bond",
+		Saying: "Shaken, not stirred.",
+	}
+	
+	p2 := person{
+		First: "Miss",
+		Last: "Moneypenny",
+		Saying: "Hello, James. It is sooooooooo good to see you.",
+	}
+
+	xp := []person{p1, p2}
+	tpl.ExecuteTemplate(w, "index.gohtml", xp)
+}
---
+++ 000_temp/46_sp17/52-review/08/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+	"html/template"
+)
+
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(http.Dir("public"))))
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	s := "Paradise Found"
+	tpl.ExecuteTemplate(w, "index.gohtml", s)
+}
