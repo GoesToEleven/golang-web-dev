a57c9895f923bb9225f733eb8706eb2f432b4973
Todd McLeod <tuddleymc@gmail.com>
2017-07-21 08:49:36 -0700

    bcrypt example

---
+++ 000_temp/51_bcrypt/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"golang.org/x/crypto/bcrypt"
+	"fmt"
+)
+
+func main() {
+	s := `password123`
+	bs, err := bcrypt.GenerateFromPassword([]byte(s), bcrypt.MinCost)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(s)
+	fmt.Println(bs)
+
+	loginPword1 := `password1234`
+
+	err = bcrypt.CompareHashAndPassword(bs, []byte(loginPword1))
+	if err != nil {
+		fmt.Println("YOU CAN'T LOGIN")
+		return
+	}
+
+	fmt.Println("You're logged in")
+}
