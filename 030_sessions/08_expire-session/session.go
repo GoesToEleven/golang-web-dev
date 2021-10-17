24bf405788c64f243015bdef4f945ca3c2499618
Tim Garrity <40044553+Salamandastron1@users.noreply.github.com>
2021-10-17 15:54:12 -0400

    NewV4() returns one value

--- 030_sessions/08_expire-session/session.go
+++ 030_sessions/08_expire-session/session.go
@@ -11,7 +11,7 @@ func getUser(w http.ResponseWriter, req *http.Request) user {
 	// get cookie
 	c, err := req.Cookie("session")
 	if err != nil {
-		sID, _ := uuid.NewV4()
+		sID := uuid.NewV4()
 		c = &http.Cookie{
 			Name:  "session",
 			Value: sID.String(),
