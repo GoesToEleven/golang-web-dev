12da0976064543a382ddd080d7341f1ca9ff8875
PolymorphicPanther <38185025+Luveshen@users.noreply.github.com>
2022-08-31 09:41:37 +0200

    Remove unused person struct

--- 027_passing-data/06_enctype/01_default/main.go
+++ 027_passing-data/06_enctype/01_default/main.go
@@ -12,12 +12,6 @@ func init() {
 	tpl = template.Must(template.ParseGlob("templates/*"))
 }
 
-type person struct {
-	FirstName  string
-	LastName   string
-	Subscribed bool
-}
-
 func main() {
 	http.HandleFunc("/", foo)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
